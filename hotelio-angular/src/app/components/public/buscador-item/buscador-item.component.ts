5d0b2ab55f21afecdfd59eeff8f78015443f0b0c
josepmartorell <martorelljosep>
2022-09-22 14:05:15 +0200

    actualización buscador-mapa e incorporado componente buscador-item

---
+++ hotelio-angular/src/app/components/public/buscador-item/buscador-item.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-buscador-item',
+  templateUrl: './buscador-item.component.html',
+  styleUrls: ['./buscador-item.component.css']
+})
+export class BuscadorItemComponent implements OnInit {
+
+  
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
